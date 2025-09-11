16b81396ac780d5fb85d96e0383d4819734d796e
GustavoLealDev <GustavoLealP@outlook.com>
2025-09-11 14:45:21 -0300

    Enviando o tratamento com todos os meses por ano

--- ProjetoBigData/Appy.py
+++ ProjetoBigData/Appy.py
@@ -1,26 +1,32 @@
 import pandas as pd
-import numpy as np
 import pyspark
 from pyspark.sql import SparkSession
 #Bibliotecas
 
-#Criando sessao do Spark
+# Criando sessao do Spark
 spark = SparkSession.builder.master("local[*]").getOrCreate()
 
-#le o arquivo e pula as 6 primeiras linhas
+# Lê o arquivo e pula as 6 primeiras linhas
 df_pandas = pd.read_csv("2295.csv", sep=",", encoding="UTF-8", skiprows=6)
 
-#Renomeando a primeira coluna
+# Renomeando a primeira coluna
 df_pandas.rename(columns={df_pandas.columns[0]: "Ano"}, inplace=True)
 
-#Converte a coluna Ano em numerico e filtra apenas linhas com valores validos
+# Converte a coluna Ano e so seliciona linhas com valores validos
 df_pandas = df_pandas[pd.to_numeric(df_pandas["Ano"], errors="coerce").notnull()]
 
-#Remove os espacos e converte para int
-df_pandas["Jan"] = df_pandas["Jan"].str.replace(" ", "").astype(int)
+# Lista dos meses 
+meses = ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"]
 
-#Seleciona apenas os Anos e Janeiro de cada ano
-dados = spark.createDataFrame(df_pandas[["Ano", "Jan"]])
+# Limpa espacos e converte para int todas as colunas
+for roubos in meses:
+    df_pandas[roubos] = df_pandas[roubos].astype(str).str.replace(" ", "").replace("", "0")
+    df_pandas[roubos] = pd.to_numeric(df_pandas[roubos], errors="coerce").fillna(0).astype(int)
 
-#Exibe as 25 primeiras linhas
+# Seleciona apenas Ano e meses
+dados = spark.createDataFrame(df_pandas[["Ano"] + meses])
+
+# Exibe as 25 primeiras linhas
 dados.show(25)
+
+dados.printSchema()
